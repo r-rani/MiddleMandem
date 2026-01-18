9ac768c238d3c72ae83afe6d0dd8db74cdc478b4
Rameen-T <tariqr9@mcmaster.ca>
2026-01-17 23:11:27 -0500

    Update ResultsView.swift with comments

--- Midz/Midz/ResultsView.swift
+++ Midz/Midz/ResultsView.swift
@@ -1,29 +1,38 @@
+//
 //  ResultsView.swift
 //  Midz
 //
-//  Created by Komal Khan on 2026-01-10.
+//  Displays the results or midpoint of selected locations.
 //
 
 import SwiftUI
 import MapKit
 
+/// Placeholder view for displaying midpoint results
 struct ResultsView: View {
+
     var body: some View {
         ZStack {
-            Color.black.ignoresSafeArea()
+            // Background color
+            Color.black
+                .ignoresSafeArea()
 
             VStack(spacing: 24) {
+
+                // App logo
                 Image("midz_logo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 100)
                     .padding(.top, 40)
                 
+                // Main title
                 Text("Find Your Midpoint")
                     .font(.largeTitle)
                     .foregroundColor(Color(hexString: "#FF2F92"))
                     .bold()
 
+                // Subtitle / placeholder
                 Text("Coming Soon!")
                     .foregroundColor(.white)
                     .font(.headline)
